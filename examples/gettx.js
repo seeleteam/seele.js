72057048e2b0dcdffa177d49cd388eb77a6179b2
Wang Feifan <wangfeifan@zsbatech.com>
2018-10-13 13:47:12 +0800

    add filterBlockTx

---
+++ examples/gettx.js
@@ -0,0 +1,12 @@
+var seeleWebProvider  = require('../src/seele');
+
+var client =new seeleWebProvider();
+
+client.send("getTransactionByHash", "0x579786960cbb87f0a344842c5258bf45b04b084a6b966e2841710d14b5c69575", function (err, tx) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(tx)
+});

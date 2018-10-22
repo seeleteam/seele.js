007580e394f8f214a34731fe960807ffd0598bc2
Wang Feifan <wangfeifan@zsbatech.com>
2018-10-22 15:22:56 +0800

    add error handle example

--- examples/error.js
+++ examples/error.js
@@ -7,6 +7,8 @@ var client = new SeeleWebProvider();
 client.send("getTransactionByHash", "0x579786960cbb87f0a344842c5258bf45b04b084a6b966e2841710d14b5c69575", function (err, tx) {
     if (err){
 		console.log(err)
+		var esg = JSON.parse(err.message)
+		console.log(esg.error.message)
 		return
 	}
 	

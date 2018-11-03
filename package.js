b8c4aa97f81f84af04a2817f764f43cc38f523fb
WTer <wesleyliu@qq.com>
2018-11-03 16:57:04 +0800

    fix a typo

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'wangff:seelejs',
   version: '0.0.8',
   // Brief, one-line summary of the package.
-  summary: 'Seele JavaScript API, middleware for communicating with the seee node via RPC',
+  summary: 'Seele JavaScript API, middleware for communicating with the Seele nodes via RPC',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/seeleteam/seelejs.git',
   // By default, Meteor will default to using README.md for documentation.
